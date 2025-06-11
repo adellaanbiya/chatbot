c419d6d9fdeee6df024807f8ebaa7aaac58fe821
Adela Nur Anbiya <adellanur899@gmail.com>
2025-06-11 22:13:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from chatbot import get_response_from_model
-import os
 
 app = Flask(__name__)
 CORS(app)
@@ -26,5 +25,4 @@ def chat():
         return jsonify({"answer": "Tidak ada pesan yang diterima."})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
